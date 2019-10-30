6173418644ddc8ee46d085c8cab83c7cfea3c5ab
ookangzheng <okz5289@gmail.com>
2019-10-30 16:45:46 +0800

    add gitignore

--- hosts/convert.py
+++ hosts/convert.py
@@ -1,10 +1,10 @@
 import os
 
 inputFile = "gravity.list"
-outputFile = "rpz.blacklist"
+outputFile = "rpz.txt"
 
 with open(inputFile, 'r') as f: # load file
     lines = f.read().splitlines()  # read lines
-os.remove("rpz.blacklist")
+os.remove("rpz.txt")
 with open(outputFile, 'w') as f:
     f.write('\n'.join([line + '  CNAME .' for line in lines]))
